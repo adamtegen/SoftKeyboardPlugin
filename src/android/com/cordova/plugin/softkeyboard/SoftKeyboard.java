c0fa9f30b5bdf6fe6c3919ef9a3b30735dcf9b85
Adam tegen <adam.tegen@gmail.com>
2015-04-06 19:33:18 -0500

    Attempt to fix crosswalk based on https://crosswalk-project.org/jira/browse/XWALK-1814

--- src/android/com/cordova/plugin/softkeyboard/SoftKeyboard.java
+++ src/android/com/cordova/plugin/softkeyboard/SoftKeyboard.java
@@ -25,10 +25,16 @@ public class SoftKeyboard extends CordovaPlugin {
     }
 
     public void showKeyBoard() {
-        InputMethodManager mgr = (InputMethodManager) cordova.getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
-        mgr.showSoftInput(webView.getView(), InputMethodManager.SHOW_IMPLICIT);
+        cordova.getActivity().runOnUiThread(new Runnable() {
+            public void run()
+            { 
+                webView.requestFocus(); 
+                InputMethodManager mgr = (InputMethodManager) cordova.getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                mgr.showSoftInput(webView.getView(), InputMethodManager.SHOW_IMPLICIT);
 
-        ((InputMethodManager) cordova.getActivity().getSystemService(Context.INPUT_METHOD_SERVICE)).showSoftInput(webView.getView(), 0);
+                ((InputMethodManager) cordova.getActivity().getSystemService(Context.INPUT_METHOD_SERVICE)).showSoftInput(webView.getView(), 0);
+            }
+        });
     }
 
     public void hideKeyBoard() {
