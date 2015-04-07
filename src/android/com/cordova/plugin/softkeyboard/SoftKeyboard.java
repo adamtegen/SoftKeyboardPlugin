57ef67a55bc5cf8b69ad53a2755b93bb78840452
Adam tegen <adam.tegen@gmail.com>
2015-04-06 21:17:00 -0500

    Fixed compiler error

--- src/android/com/cordova/plugin/softkeyboard/SoftKeyboard.java
+++ src/android/com/cordova/plugin/softkeyboard/SoftKeyboard.java
@@ -28,7 +28,7 @@ public class SoftKeyboard extends CordovaPlugin {
         cordova.getActivity().runOnUiThread(new Runnable() {
             public void run()
             {   
-                webView.getView().setFocusableInTouchMode(true)
+                webView.getView().setFocusableInTouchMode(true);
                 webView.getView().requestFocus();
                 InputMethodManager mgr = (InputMethodManager) cordova.getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
                 mgr.showSoftInput(webView.getView(), InputMethodManager.SHOW_IMPLICIT);
